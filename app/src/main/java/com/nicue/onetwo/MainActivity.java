202376265dbf6c4f35fca67783788a76ca80fee2
dcutullic <dcutullic@gmail.com>
2020-01-30 22:22:01 +0100

    For the chooser fragment keep display of selected players even if any player release finger. Add a reset button to launch another choose session

--- app/src/main/java/com/nicue/onetwo/MainActivity.java
+++ app/src/main/java/com/nicue/onetwo/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Toolbar toolbar;
     private Switch mSwitch;
     private Button rollAllButton;
+    private Button resetButton;
     private Button addTimerButton;
     private Button delTimerButton;
     private TextView actionTitle;
@@ -52,11 +53,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // Set a Toolbar to replace the ActionBar.
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         mSwitch = (Switch) toolbar.findViewById(R.id.toolbar_switch);
+        resetButton = (Button) toolbar.findViewById(R.id.reset_chosen_button);
         rollAllButton = (Button) toolbar.findViewById(R.id.roll_dices_button);
         addTimerButton = (Button) toolbar.findViewById(R.id.add_timer_button);
         delTimerButton = (Button) toolbar.findViewById(R.id.remove_timer_button);
         actionTitle = (TextView) toolbar.findViewById(R.id.custom_toolbar_title);
         mSwitch.setOnCheckedChangeListener(this);
+        resetButton.setOnClickListener(this);
         rollAllButton.setOnClickListener(this);
         addTimerButton.setOnClickListener(this);
         delTimerButton.setOnClickListener(this);
@@ -98,6 +101,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             }else if (tempFragment instanceof ChooserFragment){
                 currentTitleString = getString(R.string.menu_chooser);
+                resetButton.setVisibility(View.VISIBLE);
                 mSwitch.setVisibility(View.VISIBLE);
 
             }else if (tempFragment instanceof TimerFragment){
@@ -153,6 +157,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.remove_timer_button:
                 delTimer();
                 break;
+            case R.id.reset_chosen_button:
+                resetChooser();
+                break;
+
         }
     }
 
@@ -179,6 +187,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.nav_first_fragment:
                 fragmentClass = CounterFragment.class;
                 mSwitch.setVisibility(View.INVISIBLE);
+                resetButton.setVisibility(View.INVISIBLE);
                 rollAllButton.setVisibility(View.INVISIBLE);
                 delTimerButton.setVisibility(View.INVISIBLE);
                 addTimerButton.setVisibility(View.INVISIBLE);
@@ -186,6 +195,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.nav_second_fragment:
                 fragmentClass = DiceFragment.class;
                 mSwitch.setVisibility(View.INVISIBLE);
+                resetButton.setVisibility(View.INVISIBLE);
                 rollAllButton.setVisibility(View.VISIBLE);
                 delTimerButton.setVisibility(View.INVISIBLE);
                 addTimerButton.setVisibility(View.INVISIBLE);
@@ -193,6 +203,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.nav_third_fragment:
                 fragmentClass = ChooserFragment.class;
                 mSwitch.setVisibility(View.VISIBLE);
+                resetButton.setVisibility(View.VISIBLE);
                 rollAllButton.setVisibility(View.INVISIBLE);
                 delTimerButton.setVisibility(View.INVISIBLE);
                 addTimerButton.setVisibility(View.INVISIBLE);
@@ -201,6 +212,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.nav_fourth_fragment:
                 fragmentClass = TimerFragment.class;
                 mSwitch.setVisibility(View.INVISIBLE);
+                resetButton.setVisibility(View.INVISIBLE);
                 rollAllButton.setVisibility(View.INVISIBLE);
                 delTimerButton.setVisibility(View.VISIBLE);
                 addTimerButton.setVisibility(View.VISIBLE);
@@ -271,6 +283,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    public void resetChooser(){
+        Fragment tempFragment = getSupportFragmentManager().findFragmentById(R.id.m_content);
+
+        if (tempFragment instanceof ChooserFragment){
+            ((ChooserFragment) tempFragment).resetChooser();
+        }
+    }
     public void loadTimersData(){
         Fragment tempFragment2 = getSupportFragmentManager().findFragmentById(R.id.m_content);
 
