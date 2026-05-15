9e68aafc565cf4f000f0f2dc75cf17d2415dc2da
NICOLAS LEYTON <ndleyton@uc.cl>
2026-05-15 18:11:38 -0400

    Move chooser order number outside the touch circle

--- app/src/main/java/com/nicue/onetwo/utils/TouchDisplayView.java
+++ app/src/main/java/com/nicue/onetwo/utils/TouchDisplayView.java
@@ -3,7 +3,6 @@ package com.nicue.onetwo.utils;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
-import android.content.res.Configuration;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -13,6 +12,7 @@ import android.util.AttributeSet;
 import android.util.SparseArray;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
 import android.os.Vibrator;
 
@@ -29,11 +29,11 @@ public class TouchDisplayView extends View {
     private boolean alreadyChosen = false;
     private boolean choosingOrder = false;
     //private int fingers = 0;
-    private Random random = new Random();
     private static final long SELECTION_REVEAL_DURATION_MS = 650L;
     private static final int SELECTED_TOUCH_ALPHA = 255;
     private static final int DIMMED_TOUCH_ALPHA = 105;
     private static final int TOUCH_HALO_ALPHA = 125;
+    private static final float ORDER_LABEL_OFFSET_DP = 22f;
     private int chosenColor = 0;
     private int chosenId = -1;
     private int[] randomArray = {};
@@ -43,12 +43,24 @@ public class TouchDisplayView extends View {
     private float selectionRevealMaxRadius = 0f;
     private ValueAnimator selectionRevealAnimator;
 
+    private float pulseValue = 0f;
+    private ValueAnimator pulseAnimator;
+
     private int backgroundColorOverride = getResources().getColor(R.color.overrideBackground);
 
 
     private final int[] COLORS = {
-            0xFF03A9F4, 0xFF009688, 0xFF8BC34A, 0xFFF44336, 0xFFFF9800,
-            0xFFFF5722, 0xFF795548, 0xFFAFB3A0, 0xFFE91E63, 0xFF9C27B0};
+            0xFF6750A4, // M3 Purple
+            0xFF0061A4, // M3 Blue
+            0xFF006A6A, // M3 Teal
+            0xFF386A20, // M3 Green
+            0xFF8B5000, // M3 Orange
+            0xFFBA1A1A, // M3 Red
+            0xFF984061, // M3 Pink
+            0xFF4A6572, // M3 Blue Gray
+            0xFF525E75, // M3 Steel
+            0xFF7D5260  // M3 Maroon
+    };
 
 
     //A Handler to check if all fingers have been pressed down for x time
@@ -69,6 +81,7 @@ public class TouchDisplayView extends View {
                 chosenColor = COLORS[chosenId % COLORS.length];
                 updateSelectionRevealBounds();
                 startSelectionRevealAnimation();
+                startPulseAnimation();
                 Vibrator v = (Vibrator) getContext().getSystemService(Context.VIBRATOR_SERVICE);
                 long[] pattern = {0,20,10,50};
                 v.vibrate(pattern, -1);
@@ -160,6 +173,13 @@ public class TouchDisplayView extends View {
         mTouches = new SparseArray<TouchHistory>(10);
 
         initialisePaint();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        handler.removeCallbacks(runnable);
+        resetSelection();
+        super.onDetachedFromWindow();
     }
 
     // BEGIN_INCLUDE(onTouchEvent)
@@ -322,12 +342,15 @@ public class TouchDisplayView extends View {
 
     // calculated radiuses in px
     private float mCircleRadius;
-    private int mScreenWidth;
+    private float mPulseBaseOffset;
+    private float mPulseOffset;
+    private float mPulseFadeRange;
+    private float mOrderLabelOffset;
 
-    private Paint mCirclePaint = new Paint();
-    private Paint mTextPaint = new Paint();
-    private Paint mStrokePaint = new Paint();
-    private Paint mTransStrokePaint = new Paint();
+    private Paint mCirclePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Paint mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Paint mStrokePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Paint mTransStrokePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private Paint mRevealPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
 
@@ -339,23 +362,24 @@ public class TouchDisplayView extends View {
         // Calculate radiuses i n px from dp based on screen density
         float density = getResources().getDisplayMetrics().density;
         mCircleRadius = CIRCLE_RADIUS_DP * density;
-        //we want to know the screen width to make the white text remain in the screen
-        Configuration configuration = getContext().getResources().getConfiguration();
-        mScreenWidth = configuration.smallestScreenWidthDp * (int) density ;
-        //mCircleHistoricalRadius = CIRCLE_HISTORICAL_RADIUS_DP * density;
+        mPulseBaseOffset = 35f * density;
+        mPulseOffset = 20f * density;
+        mPulseFadeRange = 180f * density;
+        mOrderLabelOffset = ORDER_LABEL_OFFSET_DP * density;
 
         // Setup text paint for circle label
-        mTextPaint.setTextSize(30f*density);
-        mStrokePaint.setTextSize(30f*density);
+        mTextPaint.setTextSize(18f*density);
         mTextPaint.setColor(Color.WHITE);
-        mStrokePaint.setColor(Color.WHITE);
-        mTransStrokePaint.setColor(Color.WHITE);
-        mTextPaint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
-        mStrokePaint.setStyle(Paint.Style.STROKE);
-        mStrokePaint.setStrokeWidth(6f* density);
-        mTransStrokePaint.setStyle(Paint.Style.STROKE);
-        mTransStrokePaint.setStrokeWidth(6f* density);
+        mTextPaint.setTypeface(Typeface.create("sans-serif-medium", Typeface.NORMAL));
+        mTextPaint.setTextAlign(Paint.Align.CENTER);
 
+        mStrokePaint.setColor(Color.WHITE);
+        mStrokePaint.setStyle(Paint.Style.STROKE);
+        mStrokePaint.setStrokeWidth(3f* density);
+
+        mTransStrokePaint.setColor(Color.WHITE);
+        mTransStrokePaint.setStyle(Paint.Style.STROKE);
+        mTransStrokePaint.setStrokeWidth(3f* density);
     }
 
     protected void drawCircle(Canvas canvas, int id, TouchHistory data) {
@@ -377,40 +401,15 @@ public class TouchDisplayView extends View {
 
 
         if(alreadyChosen){
-            int place = indexInArray(randomArray, id) +1;
             if (chosenId == id){
-                //We draw he circle first to be under the text
-                canvas.drawCircle(data.x, (data.y) - half_r, radius + 10,
-                        mStrokePaint);
-                //Here we can add more circles for more visibility of chosen one;
-                mTransStrokePaint.setAlpha(125);
-                canvas.drawCircle(data.x, (data.y) - half_r, radius + 50,
-                        mTransStrokePaint);
-                mTransStrokePaint.setAlpha(50);
-                canvas.drawCircle(data.x, (data.y) - half_r, radius + 90,
-                        mTransStrokePaint);
-                if ((data.x + radius + 50)<mScreenWidth ) {
-                    canvas.drawText("Chosen", data.x + radius, data.y
-                            - radius, mTextPaint);
-                }else{
-                    canvas.drawText("Chosen", data.x - radius -100, data.y
-                            - radius - 90, mTextPaint);
-                }
-
+                drawPulsingConcentricCircles(canvas, data.x, data.y, radius, half_r);
                 drawBig = false;
             }else{ // With this line we are giving it a random order
                 if (choosingOrder) {
-                    if ((data.x + radius + 20)<mScreenWidth ) {
-                        canvas.drawText(String.valueOf(place), data.x + radius + 20, data.y
-                                - radius - 20, mTextPaint);
-                    }else{
-                        canvas.drawText(String.valueOf(place), data.x - radius-40, data.y
-                                        - radius -20, mTextPaint);
-                    }
-
-                    // uncomment this to add a second indicator of order
-                    //canvas.drawText(String.valueOf(place), data.x - radius-20, data.y
-                    //        + radius +20, mTextPaint);
+                    int place = indexInArray(randomArray, id) + 1;
+                    mTextPaint.setColor(Color.WHITE);
+                    canvas.drawText(String.valueOf(place), data.x,
+                            data.y - half_r - radius - mOrderLabelOffset, mTextPaint);
                     drawBig = false;
                 }
             }
@@ -487,6 +486,40 @@ public class TouchDisplayView extends View {
         return (float) Math.sqrt((xDiff * xDiff) + (yDiff * yDiff));
     }
 
+    private void startPulseAnimation() {
+        if (pulseAnimator != null) {
+            pulseAnimator.cancel();
+        }
+        pulseAnimator = ValueAnimator.ofFloat(0f, 1f);
+        pulseAnimator.setDuration(1200L);
+        pulseAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        pulseAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        pulseAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        pulseAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                pulseValue = (Float) animation.getAnimatedValue();
+                invalidate();
+            }
+        });
+        pulseAnimator.start();
+    }
+
+    private void drawPulsingConcentricCircles(Canvas canvas, float x, float y, float radius, float half_r) {
+        for (int i = 0; i < 3; i++) {
+            float baseOffset = (i + 1) * mPulseBaseOffset;
+            float pulseOffset = pulseValue * mPulseOffset;
+            float circleRadius = radius + baseOffset + pulseOffset;
+            
+            // Fade out as they get larger
+            int alpha = (int) (160 * (1.0f - (circleRadius - radius) / (radius + mPulseFadeRange)));
+            if (alpha < 0) alpha = 0;
+            
+            mTransStrokePaint.setAlpha(alpha);
+            canvas.drawCircle(x, y - half_r, circleRadius, mTransStrokePaint);
+        }
+    }
+
     private void resetSelection() {
         alreadyChosen = false;
         chosenId = -1;
@@ -499,6 +532,11 @@ public class TouchDisplayView extends View {
             selectionRevealAnimator.cancel();
             selectionRevealAnimator = null;
         }
+        if (pulseAnimator != null) {
+            pulseAnimator.cancel();
+            pulseAnimator = null;
+        }
+        pulseValue = 0f;
     }
 
     public int indexInArray(int[] arr, int n){
