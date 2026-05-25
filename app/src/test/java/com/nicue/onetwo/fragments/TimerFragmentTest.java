96a9e73185ddb025e4220b7ae51b8b019c602d61
NICOLAS LEYTON <ndleyton@uc.cl>
2026-05-24 21:17:47 -0400

    Use available height to cap timer rows

--- app/src/test/java/com/nicue/onetwo/fragments/TimerFragmentTest.java
+++ app/src/test/java/com/nicue/onetwo/fragments/TimerFragmentTest.java
@@ -1,15 +1,22 @@
 package com.nicue.onetwo.fragments;
 
+import static org.junit.Assert.assertEquals;
+
 import com.nicue.onetwo.ui.timer.TimerFragment;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
 
 public class TimerFragmentTest {
 
     @Test
-    public void calculateMaxTimersUsesAvailableScreenHeight() {
+    public void calculateMaxTimersUsesAvailableTimerHeight() {
         assertEquals(2, TimerFragment.calculateMaxTimers(200));
         assertEquals(8, TimerFragment.calculateMaxTimers(700));
+    }
+
+    @Test
+    public void calculateMaxTimersRequiresFullTimerRows() {
+        assertEquals(1, TimerFragment.calculateMaxTimers(77));
+        assertEquals(1, TimerFragment.calculateMaxTimers(78));
+        assertEquals(2, TimerFragment.calculateMaxTimers(156));
     }
 }
